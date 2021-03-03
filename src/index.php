18ad520eb10bc7d9dc09c4b4bab039ba0d951764
xinwo <1662030+xinwo@users.noreply.github.com>
2021-03-03 22:27:29 +0900

    Update index.php

--- src/index.php
+++ src/index.php
@@ -19,13 +19,7 @@
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-                <?php
-                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
-                        echo fread($myfile,filesize("/var/www/my-vol/date"));
-                        fclose($myfile);
-                ?>
                 <p>Version updated</p>
-
             </div>
         </div>
 
